22623cd023e26313977b2b5ab3e45e6c3112ccfd
Uros Cirkovic <cirkovic.uros.srb@gmail.com>
2019-11-18 12:49:44 +0100

    Update

--- routes/books.js
+++ routes/books.js
@@ -1,14 +1,33 @@
 var express = require('express');
 var router = express.Router();
 
+const Book = require('./../models/Book');
+
 // GET   /books
 router.get('/', function(req, res, next) {
-  res.render('books');
+  Book.find()
+    .then(allBooksFromDB => {
+      res.render('books', { allBooksFromDB });
+    })
+    .catch(err => console.log(err));
 });
 
-// GET /books/add
+// GET /books/add     --RENDERS THE FORM
 router.get('/add', (req, res, next) => {
   res.render('book-add');
+});
+
+// POST /books/add  -- HANDLES INCOMING DATA
+router.post('/add', (req, res, next) => {
+  console.log(req.body);
+
+  const { title, author, description, rating } = req.body;
+
+  Book.create({ title, author, description, rating })
+    .then(book => {
+      res.redirect('/books');
+    })
+    .catch(err => console.log(err));
 });
 
 module.exports = router;
